0f4fe733257513456b883ceab59ebb7f8cedfdf9
jmccormac01 <jmccormac001@gmail.com>
2016-01-11 09:29:46 +0000

    making some tweaks to how exposure is controlled

--- alcor.py
+++ alcor.py
@@ -17,17 +17,24 @@ import numpy as np
 
 config="/home/ops/fswebcam/paranal.conf"
 live_image="/home/ops/webcam/allsky-large.jpeg"
-flags='-i 0 -l 30 --gmt --no-banner' # try using --read to see if that helps
+flags='-i 0 --gmt --no-banner' # try using --read to see if that helps
 
 def adjustExptime(av,texp):
+	
+	# don't bother if already good	
+	if av < 200 and av > 150:
+		adjust=False
+		print "Current texp is fine, not adjusting"
+		return adjust,texp
+
 	texp_min=1
 	texp_max=120000
-	target_adu=150
+	target_adu=175
 	diff=target_adu/av
 	
 	# some sanity checks on low/high counts
 	if av < 10:
-		diff=2
+		diff=1.5
 	elif av >240:
 		diff=0.5
 
@@ -44,14 +51,15 @@ def adjustExptime(av,texp):
 	elif new_texp<texp_min:
 		new_texp=texp_min
 	print "New exptime: %d" % (new_texp)
-	return new_texp
+	adjust=True
+	return adjust,new_texp
 
 def getImgAverage(image_id):
 	img=Image.open(image_id)
 	x,y=img.size
-	# get a 200 pixel box in the middle for exposure control
+	# get a 400 pixel box in the middle for exposure control
 	# left, upper, right, lower - from top left
-	box=((x/2)-100,(y/2)-100,(x/2)+100,(y/2)+100)
+	box=((x/2)-200,(y/2)-200,(x/2)+200,(y/2)+200)
 	region=img.crop(box)
 	data=np.asarray(region)
 	av=np.average(data)
@@ -60,11 +68,15 @@ def getImgAverage(image_id):
 
 def main():
 	texp=100
+	adjust=True
 	while(1):
-		os.system('fswebcam -c %s -s "exposure (absolute)"="%d" %s' % (config,texp,flags))
+		if adjust:
+			os.system('fswebcam -c %s -s "exposure (absolute)"="%d" %s' % (config,texp,flags))
+		else:
+			os.system('fswebcam -c %s %s' % (config,flags))
 		time.sleep(5)
 		av=getImgAverage(live_image)
-		texp=adjustExptime(av,texp)
+		adjust,texp=adjustExptime(av,texp)
 		
 
 if __name__ == '__main__':
