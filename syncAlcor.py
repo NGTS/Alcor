e1d57b85efe2dc01088ac59f8319fde4a80fc2f2
jmccormac01 <jmccormac001@gmail.com>
2016-03-13 19:37:34 +0000

    adding checking in with centralHub to the syncing script also added a log of the last image so we can be sure that skywatcher is down if the sync script has nothing to update

--- syncAlcor.py
+++ syncAlcor.py
@@ -1,14 +1,35 @@
 #!/usr/bin/python
 # script to push the alcor images to staging 
 # and the monitor page image directory every 5 mins
-import os
+import os, Pyro4
 import glob as g
-os.chdir('/cygdrive/c/Users/ops/Documents/skywatch/')
+
+skywatch_dir='/cygdrive/c/Users/ops/Documents/skywatch'
+
+def getLastImage():
+	return open('%s/lastimg.txt' % (skywatch_dir)).readline().split('\n')[0]
+
+def setLastImage(image_id):
+	f=open('%s/lastimg.txt' % (skywatch_dir),'w')
+	f.write(image_id)
+	f.close()
+
+os.chdir(skywatch_dir)
+lastimg=getLastImage()
 current_dir=sorted(g.glob('*-*-*'))[-1]
 print('Moving to %s' % current_dir)
 os.chdir(current_dir)
 t=g.glob('*.jpg')
-print("Archiving image %s" % (t[-1]))
-os.system('scp %s ops@10.2.5.32:/ngts/staging/archive/allskycam/' % (t[-1]))
-print("Passing image %s to monitor page" % (t[-1]))
-os.system('scp %s ops@10.2.5.32:/home/ops/ngts/prism/monitor/img/allsky.jpg' % (t[-1]))
+if t[-1] != lastimg:
+	print("Archiving image %s" % (t[-1]))
+	os.system('scp %s ops@10.2.5.32:/ngts/staging/archive/allskycam/' % (t[-1]))
+	print("Passing image %s to monitor page" % (t[-1]))
+	os.system('scp %s ops@10.2.5.32:/home/ops/ngts/prism/monitor/img/allsky.jpg' % (t[-1]))
+	print("Checking in with centralHub")
+	hub = Pyro4.Proxy('PYRONAME:central.hub')
+	hub.report_in('alcor')
+	print("Done!")
+else:
+	print('%s has not updated, skipping...' % (t[-1])) 
+	print("Skipping check in with centralHub...")
+
