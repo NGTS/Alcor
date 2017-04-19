1ef575595c30e1851e99033d2b70813d223f7ee1
James McCormac <jmccormac001@gmail.com>
2017-04-19 23:50:03 +0100

    resizing to smaller sizes

--- resizeAllSkyImage.py
+++ resizeAllSkyImage.py
@@ -4,4 +4,4 @@ if __name__ == "__main__":
     os.chdir("/ngts/staging/archive/allskycam/")
     img = "allsky.jpg"
     new_img = "allsky_s.jpg"
-    os.system('/usr/local/bin/convert %s -resize 50%% %s' % (img, new_img))
+    os.system('/usr/local/bin/convert %s -resize 35%% %s' % (img, new_img))
