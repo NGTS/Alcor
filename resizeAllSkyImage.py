378529bc3d50b6be50d0a70d2522582522103ffd
James McCormac <jmccormac001@gmail.com>
2017-04-19 23:48:27 +0100

    adding correct path

--- resizeAllSkyImage.py
+++ resizeAllSkyImage.py
@@ -1,7 +1,7 @@
+#!/usr/local/python/bin/python
 import os
-
 if __name__ == "__main__":
-    os.chdir("/home/ops/ngts/prism/monitor/img/")
+    os.chdir("/ngts/staging/archive/allskycam/")
     img = "allsky.jpg"
     new_img = "allsky_s.jpg"
     os.system('/usr/local/bin/convert %s -resize 50%% %s' % (img, new_img))
