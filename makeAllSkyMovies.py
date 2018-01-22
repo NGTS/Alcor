1cdfe64fa186216d73973b729a7da75fd874e699
James McCormac <jmccormac001@gmail.com>
2018-01-21 23:54:55 -0300

    adding multithreads to script

--- makeAllSkyMovies.py
+++ makeAllSkyMovies.py
@@ -4,10 +4,12 @@ from NGTS using ffmpeg
 """
 import os
 import glob as g
+import multiprocessing as mp
 
 # pylint: disable=invalid-name
 
 if __name__ == "__main__":
+    n_cpu = mp.cpu_count()
     data_dir = '/ngts/staging/archive/allskycam'
     movie_dir = '{}/movies'.format(data_dir)
     os.chdir(data_dir)
@@ -21,8 +23,8 @@ if __name__ == "__main__":
             l1 = g.glob('*.jpeg')
             l2 = g.glob('*.jpg')
             if len(l1) > len(l2):
-                cmd = "ffmpeg -y -i '%*.jpeg' -r 30 -q:v 2 {}".format(output_file)
+                cmd = "ffmpeg -y -i '%*.jpeg' -r 30 -q:v 2 -threads {} {}".format(n_cpu, output_file)
             else:
-                cmd = "ffmpeg -y -i '%*.jpg' -r 30 -q:v 2 {}".format(output_file)
+                cmd = "ffmpeg -y -i '%*.jpg' -r 30 -q:v 2 -threads {} {}".format(n_cpu, output_file)
             os.system(cmd)
             os.chdir('../')
