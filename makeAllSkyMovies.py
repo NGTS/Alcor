024f828b48695fbac0db43cf00211421e59af7e2
James McCormac <jmccormac001@gmail.com>
2018-01-21 23:39:51 -0300

    adding check for movie and skip if exists

--- makeAllSkyMovies.py
+++ makeAllSkyMovies.py
@@ -5,19 +5,24 @@ from NGTS using ffmpeg
 import os
 import glob as g
 
+# pylint: disable=invalid-name
+
 if __name__ == "__main__":
     data_dir = '/ngts/staging/archive/allskycam'
     movie_dir = '{}/movies'.format(data_dir)
     os.chdir(data_dir)
-    templist = sorted(g.glob('2016-02-*'))
+    templist = sorted(g.glob('2016*'))
     for i in templist:
-        os.chdir(i)
-        # get the file extension
-        l1 = g.glob('*.jpeg')
-        l2 = g.glob('*.jpg')
-        if len(l1) > len(l2):
-            cmd = "ffmpeg -y -i '%*.jpeg' -r 30 -q:v 2 {}/{}.mp4".format(movie_dir, i)
-        else:
-            cmd = "ffmpeg -y -i '%*.jpg' -r 30 -q:v 2 {}/{}.mp4".format(movie_dir, i)
-        os.system(cmd)
-        os.chdir('../')
+        print('\nMaking all-sky movie for {}\n'.format(i))
+        output_file = "{}/{}.mp4".format(movie_dir, i)
+        if not os.path.exists(output_file):
+            os.chdir(i)
+            # get the file extension
+            l1 = g.glob('*.jpeg')
+            l2 = g.glob('*.jpg')
+            if len(l1) > len(l2):
+                cmd = "ffmpeg -y -i '%*.jpeg' -r 30 -q:v 2 {}".format(output_file)
+            else:
+                cmd = "ffmpeg -y -i '%*.jpg' -r 30 -q:v 2 {}".format(output_file)
+            os.system(cmd)
+            os.chdir('../')
