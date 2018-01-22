26e39ccf67beec2cc164e76ae0e9580499195555
James McCormac <jmccormac001@gmail.com>
2018-01-21 22:56:33 -0300

    adding script to make movies for youtube

---
+++ makeAllSkyMovies.py
@@ -0,0 +1,16 @@
+"""
+Script to make daily movies of the all sky images
+from NGTS using ffmpeg
+"""
+import os
+import glob
+
+if __name__ == "__main__":
+    data_dir = '/ngts/staging/archive/allskycam'
+    movie_dir = '{}/movies'.format(data_dir)
+    os.chdir(data_dir)
+    templist = g.glob('2016-01-*')
+    for i in templist:
+        os.chdir(i)
+        cmd = "ffmpeg -i '%*.jpg' -r 30 -q:v 2 {}/{}.mp4".format(movie_dir, i)
+        os.chdir('../')
